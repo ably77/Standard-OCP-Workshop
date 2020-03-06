d358452445df16478a15de20ac203f36ca90907e
ably77 <alexbrucely@gmail.com>
2020-03-06 07:21:21 -0800

    Update install.sh

--- Lab1-Cluster_Installation/install.sh
+++ Lab1-Cluster_Installation/install.sh
@@ -15,7 +15,7 @@ mkdir ${CLUSTER_PATH}/${CLUSTER_NAME}
 cp ${CLUSTER_PATH}/Standard-OCP-Workshop/Lab1-Cluster_Installation/install-config.yaml ${CLUSTER_PATH}/${CLUSTER_NAME}
 
 ### create cluster
-./openshift-install_4.3.2 create cluster --dir=${CLUSTER_PATH}/${CLUSTER_NAME} --log-level debug
+./openshift-install_4.2.20 create cluster --dir=${CLUSTER_PATH}/${CLUSTER_NAME} --log-level debug
 
 ### open console route
 open https://console-openshift-console.apps.${CLUSTER_NAME}.${DOMAIN_NAME}
